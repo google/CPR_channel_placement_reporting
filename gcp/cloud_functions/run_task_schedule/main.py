b6aebc21c54f60d1cf43a6af4678cc8586310404
Andrei Markin <amarkin@google.com>
2024-12-26 16:07:53 +0400

    Update cloud functions
    Change-Id: Id0f151af4170a8a0356acd688a2187acfd7f7440

--- gcp/cloud_functions/run_task_schedule/main.py
+++ gcp/cloud_functions/run_task_schedule/main.py
@@ -1,39 +1,50 @@
-from typing import Optional
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Handles running task."""
+
+# pylint: disable=C0330, g-bad-import-order, g-multiple-import
 
 import base64
-import functions_framework
-from googleapiclient.discovery import build
-from google.cloud.scheduler_v1 import (CloudSchedulerClient, RunJobRequest)
-from google.protobuf import field_mask_pb2
-import functions_framework
 import json
+
+import functions_framework
+from google.cloud.scheduler_v1 import CloudSchedulerClient, RunJobRequest
+from googleapiclient.discovery import build
 
 
 @functions_framework.cloud_event
 def handle_event(cloud_event):
-    event_data = cloud_event.data
-    project_id = event_data["subscription"].split("/")[1]
-    location = get_appengine_location(project_id)
-    if not location:
-        raise ValueError("cannot determine appengine location")
-    request_json = json.loads(
-        base64.b64decode(event_data["message"]["data"]).decode())
-    task_id = request_json['task_id']
+  """Handles running task."""
+  event_data = cloud_event.data
+  project_id = event_data['subscription'].split('/')[1]
+  if not (location := _get_appengine_location(project_id)):
+    raise ValueError('cannot determine appengine location')
+  request_json = json.loads(
+    base64.b64decode(event_data['message']['data']).decode()
+  )
+  task_id = request_json['task_id']
+  parent = f'projects/{project_id}/locations/{location}'
+  job_name = f'{parent}/jobs/{task_id}'
+  request = RunJobRequest(name=job_name)
+  CloudSchedulerClient().run_job(request)
 
-    parent = f"projects/{project_id}/locations/{location}"
-    job_name = f"{parent}/jobs/{task_id}"
-    client = CloudSchedulerClient()
-    run_job(client= client, job_name=job_name)
 
-def get_appengine_location(project_id: str) -> Optional[str]:
-    service = build('appengine', 'v1')
-    response = service.apps().get(appsId=project_id).execute()
-    if not (location_id := response.get("locationId")):
-        return None
-    if location_id in ("europe-west", "us-central"):
-        location_id = location_id + '1'
-    return location_id
-
-def run_job(client: CloudSchedulerClient, job_name: str) -> None:
-    request = RunJobRequest(name=job_name)
-    client.run_job(request)
+def _get_appengine_location(project_id: str) -> str | None:
+  service = build('appengine', 'v1')
+  response = service.apps().get(appsId=project_id).execute()
+  if not (location_id := response.get('locationId')):
+    return None
+  if location_id in ('europe-west', 'us-central'):
+    location_id = location_id + '1'
+  return location_id
